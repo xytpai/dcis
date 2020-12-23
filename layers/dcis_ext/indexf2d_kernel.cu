0e6b09427025bce20ea9411ce23ca26e034f5c96
徐昱韬 <xuyutao@bytedance.com>
2020-12-23 20:38:24 +0800

    initial

--- layers/dcis_ext/indexf2d_kernel.cu
+++ layers/dcis_ext/indexf2d_kernel.cu
@@ -18,16 +18,16 @@ at::Tensor indexf2d_forward_cuda(const at::Tensor &input, const at::Tensor &inde
     const int h = input.size(1);
     const int w = input.size(2);
     const int n = index.size(0);
-    auto output = at::zeros({b, h, w}, input.options());
-        if (output.numel() == 0) {
-                THCudaCheck(cudaGetLastError());
-                return output;
-        }
-        dim3 grid(b, h), block(w);
-        peakdet_kernel<<<grid, block>>>(
-                input.contiguous().data<float>(),
-                output.contiguous().data<float>(),
-                h, w);
+    auto output = at::zeros({n, h, w}, input.options());
+    if (output.numel() == 0) {
         THCudaCheck(cudaGetLastError());
         return output;
+    }
+    dim3 grid(b, h), block(w);
+    peakdet_kernel<<<grid, block>>>(
+        input.contiguous().data<float>(),
+        output.contiguous().data<float>(),
+        h, w);
+    THCudaCheck(cudaGetLastError());
+    return output;
 }
