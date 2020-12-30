448ed3a72fcc114e233923f32cb14416f60b48c1
徐昱韬 <xuyutao@bytedance.com>
2020-12-30 20:35:23 +0800

    initial

--- layers/losses.py
+++ layers/losses.py
@@ -10,8 +10,8 @@ def neg_loss(pred, gt):
     neg_inds = gt.lt(1).float()
     neg_weights = torch.pow(1 - gt, 4)
     loss = 0
-    pos_loss = torch.log(pred) * torch.pow(1 - pred, 2) * pos_inds
-    neg_loss = torch.log(1 - pred) * torch.pow(pred, 2) * neg_weights * neg_inds
+    pos_loss = torch.log(pred + 1e-10) * torch.pow(1 - pred, 2) * pos_inds
+    neg_loss = torch.log(1 - pred + 1e-10) * torch.pow(pred, 2) * neg_weights * neg_inds
     num_pos  = pos_inds.float().sum()
     pos_loss = pos_loss.sum()
     neg_loss = neg_loss.sum()
